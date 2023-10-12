d1d78243e16a3044a63f82c20feca2a289351259
Javier-cnc <grumpyjg95@gmail.com>
2023-10-11 22:55:42 -0400

    Add viewer images page

--- src/app/Components/images-viewer/images-viewer.component.ts
+++ src/app/Components/images-viewer/images-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-images-viewer',
@@ -9,10 +9,17 @@ export class ImagesViewerComponent {
   @Input('images')
   images: string[] | undefined = [];
 
+  @Output('clicked')
+  clicked: EventEmitter<void> = new EventEmitter();
+
   // define the position of the selected item in the elements to show
   selectionIndex: number = 0;
 
   setSelectedIndex(index: number) {
     this.selectionIndex = index;
+  }
+
+  imageClicked() {
+    this.clicked.emit();
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,10 @@ import { Product } from './models/product.model';
 export class AppComponent {
   title = 'starting-project';
   productModel$: Product | null = null;
+  isLayerVisible: boolean = false;
+
+  // defines the quantity of the current product to order by the client
+  productQuantityToOrder: number = 0;
 
   constructor(private repo: RepositoryService) {
     // create a dummy id
@@ -26,5 +30,27 @@ export class AppComponent {
       },
       complete: () => {},
     });
+  }
+
+  decreaseQuantity() {
+    if (this.productQuantityToOrder > 0) {
+      this.productQuantityToOrder--;
+    }
+  }
+
+  increaseQuantity() {
+    this.productQuantityToOrder++;
+  }
+
+  hideLayer(event: any) {
+    if (!event.clickHandled) this.isLayerVisible = false;
+  }
+
+  showLayer() {
+    this.isLayerVisible = true;
+  }
+
+  clickTesting(event: any) {
+    event.clickHandled = true;
   }
 }
