74d8c2c178122ba8e60cfd8f4a11452f4277bd1d
Javier-cnc <grumpyjg95@gmail.com>
2023-10-19 11:26:18 -0400

    fix port issue

--- backend app/app.js
+++ backend app/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // get the port number
-const port = process.environment.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // #region CORS implementation
 
